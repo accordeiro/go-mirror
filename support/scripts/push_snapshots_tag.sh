b6c7193d03ba14232bbedb999f143d67139cf49a
Alexandre Cordeiro <accordeiro@gmail.com>
2019-05-09 15:19:35 -0700

    ci: ensure snapshot releases are named properly

--- support/scripts/push_snapshots_tag.sh
+++ support/scripts/push_snapshots_tag.sh
@@ -10,6 +10,6 @@ else
     git config --global user.name "Circle CI"
     git tag -d snapshots || true
     git push --delete -q https://${GITHUB_OAUTH_TOKEN}@github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} snapshots || true
-    git tag snapshots -a -m "Generated snapshots from Circle CI for build $TRAVIS_BUILD_NUMBER on $(date --utc +'%F-%T')"
+    git tag snapshots -a -m "Generated snapshots from Circle CI for build ${CIRCLE_BUILD_NUM} on $(date --utc +'%F-%T')"
     git push -q https://${GITHUB_OAUTH_TOKEN}@github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} --tags
 fi
