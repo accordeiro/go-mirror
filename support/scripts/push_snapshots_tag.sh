e300b9c66ebe4821b4d45478d6cc54e42ab6cedb
Alexandre Cordeiro <accordeiro@gmail.com>
2019-05-09 16:03:58 -0700

    ci: document Circle CI config and add publish_artifacts job

--- support/scripts/push_snapshots_tag.sh
+++ support/scripts/push_snapshots_tag.sh
@@ -9,7 +9,7 @@ else
     git config --global user.email "builds@circleci.com"
     git config --global user.name "Circle CI"
     git tag -d snapshots || true
-    git push --delete -q https://${GITHUB_OAUTH_TOKEN}@github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} snapshots || true
+    git push --delete -q https://${GITHUB_TOKEN}@github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} snapshots || true
     git tag snapshots -a -m "Generated snapshots from Circle CI for build ${CIRCLE_BUILD_NUM} on $(date --utc +'%F-%T')"
-    git push -q https://${GITHUB_OAUTH_TOKEN}@github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} --tags
+    git push -q https://${GITHUB_TOKEN}@github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} --tags
 fi
