38b883a42280ce8e18e478a6fd8fe6ce006125cb
Alexandre Cordeiro <accordeiro@gmail.com>
2019-05-09 14:16:33 -0700

    ci: add Circle CI config to publish snapshots

--- support/scripts/push_snapshots_tag.sh
+++ support/scripts/push_snapshots_tag.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #this script will delete and recreate the snapshots tag, unless it's a tagged release
 
-if [ "$TRAVIS_TAG" != "" ]
+if [ "$CIRCLE_TAG" != "" ]
 then
     echo "tagged release - doing nothing"
 else
     echo "snapshots release - overwriting snapshots tag"
-    git config --global user.email "builds@travis-ci.com"
-    git config --global user.name "Travis CI"
+    git config --global user.email "builds@circleci.com"
+    git config --global user.name "Circle CI"
     git tag -d snapshots || true
-    git push --delete -q https://$GITHUB_OAUTH_TOKEN@github.com/stellar/go snapshots || true
-    git tag snapshots -a -m "Generated snapshots from TravisCI for build $TRAVIS_BUILD_NUMBER on $(date --utc +'%F-%T')"
-    git push -q https://$GITHUB_OAUTH_TOKEN@github.com/stellar/go --tags
+    git push --delete -q https://${GITHUB_OAUTH_TOKEN}@github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} snapshots || true
+    git tag snapshots -a -m "Generated snapshots from Circle CI for build $TRAVIS_BUILD_NUMBER on $(date --utc +'%F-%T')"
+    git push -q https://${GITHUB_OAUTH_TOKEN}@github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME} --tags
 fi
